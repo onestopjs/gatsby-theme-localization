3628fce8401c97f9c8e8ac44ad3b239c94420d84
Martin Koparanov <m.n.koparanov@gmail.com>
2019-09-08 16:34:08 +0300

    Move I18nextProvider above WrapRoot and use the useTranslation hook properly

--- gatsby-theme-localization/src/gatsby-browser.tsx
+++ gatsby-theme-localization/src/gatsby-browser.tsx
@@ -4,6 +4,7 @@ import WrapRoot from "./wrap-root";
 import getLangFromPathname from "./utils/getLangFromPathname";
 import {i18nextOptions as defaultI18nextOptions} from "./defaultOptions";
 import { PluginOptions } from "./types";
+import { I18nextProvider } from "react-i18next";
 
 const getInitialLang = (pathname: string, options: PluginOptions) => {
   const pathLang = getLangFromPathname(pathname);
@@ -29,9 +30,11 @@ export const wrapRootElement = ({ element }: any, options: any) => {
 
   return (
     <MaybeSuspense fallback="">
-      <WrapRoot i18n={i18n} options={options} >
-        {element}
-      </WrapRoot>
+      <I18nextProvider i18n={i18n}>
+        <WrapRoot options={options} >
+          {element}
+        </WrapRoot>
+      </I18nextProvider>
     </MaybeSuspense>
     
   );
